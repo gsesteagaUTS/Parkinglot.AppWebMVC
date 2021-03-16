4b8f484ae2e70155ae4eadd04315150a063ebda5
Gilberto Sesteaga <gisego03@gmail.com>
2021-03-16 14:36:56 -0700

    Se agregaron los repositorios en blanco

---
+++ Parkinglot.AppWebMVC/Repositories/ParkinglotRepository.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Parkinglot.AppWebMVC.Repositories
+{
+    public interface IParkinglotRepository
+    {
+        //CreateParkinglot
+        //UpdateParkinglot
+        //DeleteParkinglot
+        //FindParkinglot
+        //FindAllParkinglot
+        //AddSpot
+        //UpdateSpot
+        //DeleteSpot
+        //UpdatePir
+    }
+    public class ParkinglotRepository
+    {
+    }
+}
