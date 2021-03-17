a1f080ce277a1e00aac517b8653943fb266350d8
Gilberto Sesteaga <gisego03@gmail.com>
2021-03-17 16:41:29 -0700

    Configuraciones

--- Parkinglot.AppWebMVC/obj/Debug/netcoreapp3.1/Parkinglot.AppWebMVC.AssemblyInfo.cs
+++ Parkinglot.AppWebMVC/obj/Debug/netcoreapp3.1/Parkinglot.AppWebMVC.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Parkinglot.AppWebMVC")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("parkinglot.appwebmvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Parkinglot.AppWebMVC")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Parkinglot.AppWebMVC")]
+[assembly: System.Reflection.AssemblyProductAttribute("parkinglot.appwebmvc")]
+[assembly: System.Reflection.AssemblyTitleAttribute("parkinglot.appwebmvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
