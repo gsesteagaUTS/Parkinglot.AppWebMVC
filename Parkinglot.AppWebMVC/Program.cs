5d867fcdea8289b36310c2f2dbdda9fed2a1bfb8
Gilberto Sesteaga <gisego03@gmail.com>
2021-03-17 16:31:19 -0700

    Cambio de carpeta

--- Parkinglot.AppWebMVC/Program.cs
+++ Parkinglot.AppWebMVC/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace parkinglot.appwebmvc
+namespace Parkinglot.AppWebMVC
 {
     public class Program
     {
--- Parkinglot.AppWebMVC/Startup.cs
+++ Parkinglot.AppWebMVC/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace parkinglot.appwebmvc
+namespace Parkinglot.AppWebMVC
 {
     public class Startup
     {
