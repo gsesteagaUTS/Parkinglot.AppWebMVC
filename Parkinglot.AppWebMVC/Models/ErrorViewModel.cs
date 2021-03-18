237cefe38600c814675141f43757ef507eba67d9
Gilberto Sesteaga <gisego03@gmail.com>
2021-03-17 17:17:27 -0700

    Versiion 3.1

---
+++ Parkinglot.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Parkinglot.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
